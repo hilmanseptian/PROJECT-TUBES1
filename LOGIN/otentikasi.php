2c75a9d89665e70bb1428b0ca0a5ca5b1dc8c729
budi103 <budi.permana@student.unsil.ac.id>
2014-04-29 14:11:39 +0700

    Update otentikasi.php

--- LOGIN/otentikasi.php
+++ LOGIN/otentikasi.php
@@ -1,26 +1,26 @@
 <?php
 include('config.php');
  
-//tangkap data dari form login
+//data yang diambil dari form login
 $username = $_POST['username'];
 $password = $_POST['password'];
  
-//untuk mencegah sql injection
+//untuk mencegah sql injection (trik hacking mengedit isi dari variabel get yang ada di url untuk melakukan penetrasi kedalam database)
 //kita gunakan mysql_real_escape_string
 $username = mysql_real_escape_string($username);
 $password = mysql_real_escape_string($password);
  
 //cek data yang dikirim, apakah kosong atau tidak
 if (empty($username) && empty($password)) {
-    //kalau username dan password kosong
+    //jika username dan password kosong
     header('location:login.php?error=1');
     break;
 } else if (empty($username)) {
-    //kalau username saja yang kosong
+    //jika username saja yang kosong
     header('location:login.php?error=2');
     break;
 } else if (empty($password)) {
-    //kalau password saja yang kosong
+    //jika password saja yang kosong
     //redirect ke halaman index
     header('location:login.php?error=3');
     break;
@@ -29,10 +29,10 @@ if (empty($username) && empty($password)) {
 $q = mysql_query("select * from user where username='$username' and password='$password'");
  
 if (mysql_num_rows($q) == 1) {
-    //kalau username dan password sudah terdaftar di database
+    //jika username dan password sudah terdaftar di database
     header('location:index.php');
 } else {
-    //kalau username ataupun password tidak terdaftar di database
+    //jika username ataupun password tidak terdaftar di database
     header('location:login.php?error=4');
 }
 ?>
