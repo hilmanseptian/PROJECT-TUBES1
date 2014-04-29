862461369adc6448bb1886d1b56c7e96ce590e2e
Hilman Septian (117006135) <hilman.septian@student.unsil.ac.id>
2014-04-29 14:36:35 +0700

    tolong cek kembali
    note :
    action="insert.php" yaitu suatu file yang digunakan untuk memproses data yang dimasukkan melalui form ini, dan  method="post" adalah dimana suatu metode pengiriman data yang digunakan.
    Ada dua jenis menthod untuk mengirim data, yaitu "post" dan "get", Pengiriman dengan jenis method POST berarti bahwa variabel-variabel data tidak akan ditampilkan pada url tersebut, sedangkan pengiriman dengan method GET, variabel-variabel data disertakan pada url

---
+++ SimpleAppInputData/index.php
@@ -0,0 +1,49 @@
+<html>
+<head>
+<title>Belajar PHP</title>
+</head>
+ 
+<body>
+<h1>Form Input Data</h1>
+ 
+<form name="input_data" action="insert.php" method="post">
+<table border="0" cellpadding="5" cellspacing="0">
+    <tbody>
+        <tr>
+            <td>Username</td>
+            <td>:</td>
+            <td><input type="text" name="username" maxlength="20" required="required" /></td>
+        </tr>
+        <tr>
+            <td>Password</td>
+            <td>:</td>
+            <td><input type="password" name="password" maxlength="20" required="required" /></td>
+        </tr>
+        <tr>
+            <td>Fullname</td>
+            <td>:</td>
+            <td><input type="text" name="fullname" maxlength="100" required="required" /></td>
+        </tr>
+        <tr>
+            <td>Email</td>
+            <td>:</td>
+            <td><input type="email" name="email" required="required" /></td>
+        </tr>
+        <tr>
+            <td>Agama</td>
+            <td>:</td>
+            <td><input type="text" name="agama" required="required" /></td>
+        </tr>
+        <tr>
+            <td>Nomor HP</td>
+            <td>:</td>
+            <td><input type="text" name="no_hp" maxlength="14" required="required" /></td>
+        </tr>
+        <tr>
+            <td align="right" colspan="3"><input type="submit" name="submit" value="Simpan" /></td>
+        </tr>
+    </tbody>
+</table>
+</form>
+</body>
+</html>
