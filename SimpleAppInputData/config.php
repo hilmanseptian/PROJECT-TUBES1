cb12527c2e068fe9a5c473cca755918e274090e2
Hilman Septian (117006135) <hilman.septian@student.unsil.ac.id>
2014-04-29 14:29:42 +0700

    Update config.php

--- SimpleAppInputData/config.php
+++ SimpleAppInputData/config.php
@@ -1,23 +1,22 @@
 <?php
-//host yang digunakan
-//99,9% tidak perlu dirubah
+//host yang akan digunakan
+//tidak perlu dirubah
 $host = 'localhost';
  
 //username untuk login ke host
-//biasanya didapatkan pada email konfirmasi order hosting
 $user = 'root';
  
-//jika menggunakan PC sendiri sebagai host,
+//jika menggunakan LAPTOP sendiri sebagai host,
 //secara default password dikosongkan
 $pass = '';
  
-//isikan nama database sesuai database
-//yang dibuat pada langkah-1
+//isikan nama database sesuai database oke!
+//yang telah dibuat tadi
 $dbname = 'belajar';
  
-//mengubung ke host
-$connect = mysql_connects($host, $users, $pass) or die(mysql_error());
+//untuk mengubungkan ke host
+$connect = mysql_connect($host, $user, $pass) or die(mysql_error());
  
-//memilih database yang akan digunakan
-$dbselect = mysql_select_dbs($dbname);
+//untuk memilih database mana yang akan digunakan
+$dbselect = mysql_select_db($dbname);
 ?>
