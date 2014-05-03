f0732cb28d81433df7c33bf180953d8a11af7706
Hilman Septian (117006135) <hilman.septian@student.unsil.ac.id>
2014-05-03 07:37:12 +0700

    Create home.php

---
+++ home.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<style>
+p.padding
+{
+padding-top:20px;
+padding-bottom:20px;
+padding-right:40px;
+padding-left:40px;
+color:#1E90FF;
+}
+</style>
+</head>
+<body>
+	  <p align=”center” class="padding">
+	<font type="IMPACT" size=10>
+	TUGAS BESAR 1 LAYANAN WEB<br/>
+	</font>
+	</br>
+	<font size=5>
+	website ini di buat dengan alasan untuk memenuhi Salah Satu tugas yitu "Tugas Besar 1" mata kuliah Layanan Web</br>
+	</font>
+	</br>
+	<font type="Times New Roman" size=4>
+	ANGGOTA : </br>
+	117006103 Budi Permana<br />
+	117006121 Gilang Agtian<br />
+	117006133 Hendra Hendrawan<br />
+	117006135 Hilman Septian<br />
+	</font>
+ </p>
+ </body>
+ </html>
